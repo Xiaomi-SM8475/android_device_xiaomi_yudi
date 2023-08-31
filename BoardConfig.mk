f2197ca7591f857a2391946cb1b900b9158974eb
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-08-31 09:16:45 +0200

    yudi: Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2023-2024 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+include device/xiaomi/liuqin/BoardConfig.mk
+
+VARIANT_PATH := device/xiaomi/yudi
+
+# OTA
+TARGET_OTA_ASSERT_DEVICE := yudi
---
+++ device.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023-2024 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/liuqin/device.mk)
+
+# Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    device/xiaomi/yudi
