3c379fe98e1949ba481e3fe44ed3b6a908037d94
Lee HoJoon <leehojoonlee0820@gmail.com>
2023-09-21 11:13:57 +0900

    yudi: Add IDC configs for Xiaomi Pen
    Co-Authored-By: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>

--- device.mk
+++ device.mk
@@ -13,3 +13,8 @@ PRODUCT_SOONG_NAMESPACES += \
 # Display Config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/displayconfig/display_id_4630946932993367170.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946932993367170.xml
+
+# Input Device Configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/idc/Vendor_0022_Product_4d80.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_0022_Product_4d80.idc \
+    $(LOCAL_PATH)/configs/idc/Vendor_2717_Product_3802.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_2717_Product_3802.idc
