d9818c6272de34b87383f6273c361dfceb51e637
Lee HoJoon <leehojoonlee0820@gmail.com>
2023-09-21 11:02:15 +0900

    yudi: Add displayconfig file
     - Taken value from miui dump
    
     - Fixes following logspam:
       E DisplayDeviceConfig: requesting nits when no mapping exists.

--- device.mk
+++ device.mk
@@ -9,3 +9,7 @@ $(call inherit-product, device/xiaomi/liuqin/device.mk)
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     device/xiaomi/yudi
+
+# Display Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/displayconfig/display_id_4630946932993367170.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946932993367170.xml
