71f7b483f7f51b36f63010b8e0392f5c78bd0cca
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-01-26 10:42:23 +0100

    yudi: Add support for vibrations

--- device.mk
+++ device.mk
@@ -33,3 +33,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     YudiFrameworks \
     YudiSettingsProvider
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service.xiaomi
+
+PRODUCT_COPY_FILES += \
+    hardware/xiaomi/aidl/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
