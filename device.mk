ec842fedfb8ae58466529919840585cfa0d9504b
Lee HoJoon <leehojoonlee0820@gmail.com>
2024-02-20 09:46:55 +0900

    yudi: Add fingerprint sensor keylayout

--- device.mk
+++ device.mk
@@ -9,3 +9,7 @@ $(call inherit-product, device/xiaomi/liuqin/device.mk)
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     device/xiaomi/yudi
+
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
